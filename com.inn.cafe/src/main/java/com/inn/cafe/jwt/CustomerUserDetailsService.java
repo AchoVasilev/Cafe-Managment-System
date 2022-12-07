3e2f30a0e77f439860b6f866f32ea995bfb70083
Angel Vasilev <angavasilev@gmail.com>
2022-12-07 06:43:59 +0200

    add jwt

---
+++ com.inn.cafe/src/main/java/com/inn/cafe/jwt/CustomerUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.inn.cafe.jwt;
+
+import com.inn.cafe.dao.UserRepository;
+import com.inn.cafe.pojo.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomerUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+    private User user;
+
+    public CustomerUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        ;
+    }
+}
