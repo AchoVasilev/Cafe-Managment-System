7e8fb59f8cd2573aa4dc332decdfcfa845b86bf0
Angel Vasilev <angavasilev@gmail.com>
2022-12-07 22:51:12 +0200

    add config

--- com.inn.cafe/src/main/java/com/inn/cafe/jwt/CustomerUserDetailsService.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/jwt/CustomerUserDetailsService.java
@@ -5,7 +5,12 @@ import com.inn.cafe.pojo.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
+@Service
 public class CustomerUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
     private User user;
@@ -16,6 +21,16 @@ public class CustomerUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        ;
+        this.user = this.userRepository.findByEmail(username);
+        if (!Objects.isNull(this.user)) {
+            return new org.springframework.security.core.userdetails.User(
+                    this.user.getEmail(), this.user.getPassword(), new ArrayList<>());
+        }
+
+        throw new UsernameNotFoundException("User not found");
+    }
+
+    public User getUser() {
+        return this.user;
     }
 }
--- com.inn.cafe/src/main/java/com/inn/cafe/jwt/SecurityConfig.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/jwt/SecurityConfig.java
@@ -2,15 +2,42 @@ package com.inn.cafe.jwt;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web ->
+    private final CustomerUserDetailsService customerUserDetailsService;
+
+    public SecurityConfig(CustomerUserDetailsService customerUserDetailsService) {
+        this.customerUserDetailsService = customerUserDetailsService;
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http)
+            throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(this.customerUserDetailsService)
+                .passwordEncoder(this.passwordEncoder())
+                .and()
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
 }
